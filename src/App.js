bcac913252ba836184d60b70e06eb2b5dc3304d8
Erika Moraes Vaz Nascimento <erikamoraesv@gmail.com>
2020-06-14 23:42:59 -0300

    first version
    first version

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import ProjetoBase from './components/ProjetoBase/ProjetoBase';
+import { getNewTimestamp } from './hepers/timestamphelpers';
+
+export default function App () {
+  const [clickArray, setClickArray] = useState([]);
+
+  useEffect(() =>{
+    document.title = clickArray.length;
+  })
+
+  const handleClick = () => {
+    const newClickArray = Object.assign([], clickArray);
+    newClickArray.push(getNewTimestamp());
+  
+    setClickArray(newClickArray);
+  };
+
+  return(
+    <div>
+      <h1>
+        React com <em>Hooks</em>
+      </h1>
+
+      <button onClick={handleClick}>Clique aqui</button>
+
+      <ul>
+        {clickArray.map((item) => {
+          return <li key={item} >{item}</li>
+          })}
+      </ul>
+
+    </div>
+  );
+}
